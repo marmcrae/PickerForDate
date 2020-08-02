ec45086ce10f7758d2cab59d38079f18e486e68c
marmcrae <mcrae.marquie@gmail.com>
2020-08-01 18:05:54 -1000

    Initial commit

---
+++ app/src/main/java/android/studioprojects/pickerfordate/DatePickerFragment.java
@@ -0,0 +1,50 @@
+package android.studioprojects.pickerfordate;
+
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+//    public DatePickerFragment() {
+//        // Required empty public constructor
+//    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, dayOfMonth);
+
+    }
+}
